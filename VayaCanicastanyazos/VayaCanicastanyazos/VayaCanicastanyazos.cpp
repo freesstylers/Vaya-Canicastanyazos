cb1916df50efb0f94272b2517cbbecff129f77ba
Posna <jomart17@ucm.es>
2020-04-16 19:04:17 +0200

    con esto deberia ir perfecto

--- VayaCanicastanyazos/VayaCanicastanyazos/VayaCanicastanyazos.cpp
+++ VayaCanicastanyazos/VayaCanicastanyazos/VayaCanicastanyazos.cpp
@@ -1,21 +1,45 @@
-// VayaCanicastanyazos.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-#include "Entity.h"
+#include <stdexcept>
+#define NOMINMAX
 
-int main()
+#include "windows.h"
+#include "MotorCasaPaco.h"
+
+MotorCasaPaco* motorCasaPaco;
+
+#ifdef  _DEBUG
+int main(int argc, char* argv[])
+#else
+#include <Windows.h>
+int WINAPI
+WinMain(HINSTANCE hinstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow)
+#endif
 {
-    std::cout << "Hello World!\n";
+
+	try { MotorCasaPaco::setupInstance("Motor Casa Paco"); }
+	catch (const std::exception& e)
+	{
+		throw std::runtime_error("Motor Casa Paco init fail \n" + std::string(e.what()) + "\n");	return 0;
+	}
+
+	motorCasaPaco = MotorCasaPaco::getInstance();
+
+	try
+	{
+		motorCasaPaco->init();
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << e.what() << "\n";
+		delete motorCasaPaco;
+		return 0;
+	}
+
+	motorCasaPaco->changeScene("UITest");
+
+	motorCasaPaco->start();
+
+	delete motorCasaPaco;
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
