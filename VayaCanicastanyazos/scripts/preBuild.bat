07da090793efa9858b200a2a506bcef533c50327
Sergio Abreu García <saggamedeveloper@gmail.com>
2020-04-16 20:09:11 +0200

    Script eliminado y gitignore .vs

--- VayaCanicastanyazos/scripts/preBuild.bat
+++
@@ -1,7 +0,0 @@
-for /r ..\Untitled-Motor\MotorCasaPaco\dependencies\ogre-1.12.4\Build\bin\debug\ %%f in (*.dll) do @copy "%%f" ..\bin\
-for /r ..\Untitled-Motor\MotorCasaPaco\dependencies\ogre-1.12.4\Build\bin\release\ %%f in (*.dll) do @copy "%%f" ..\bin\
-for /r ..\Untitled-Motor\MotorCasaPaco\dependencies\SDL2-2.0.10\lib\x64 %%f in (*.dll) do @copy "%%f" ..\bin\
-for /r ..\Untitled-Motor\MotorCasaPaco\dependencies\fmod-2.00.07\ %%f in (*.dll) do @copy "%%f" ..\bin\
-for /r ..\Untitled-Motor\MotorCasaPaco\dependencies\bullet3-2.89\build\lib %%f in (*.dll) do @copy "%%f" ..\bin\
-for /r ..\Untitled-Motor\MotorCasaPaco\dependencies\cegui-0.8.7\build\bin\ %%f in (*.dll) do @copy "%%f" ..\bin\
-for /r ..\Untitled-Motor\MotorCasaPaco\dependencies\cegui-0.8.7\dependencies\bin %%f in (*.dll) do @copy "%%f" ..\bin\
