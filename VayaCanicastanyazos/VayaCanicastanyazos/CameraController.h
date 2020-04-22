ba6ecad017e4e1b8fa0d8d5aff15e5dfbd0e7ebc
PabloGarciaGrossi <pabgar20@ucm.es>
2020-04-22 19:55:29 +0200

    Control de Cámara

---
+++ VayaCanicastanyazos/VayaCanicastanyazos/CameraController.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Entity/Component.h"
+class CameraController : public Component
+{
+public:
+	CameraController(json& args);
+	virtual void update();
+	void init(json& args) override;
+	void redefine(json& args) override;
+	~CameraController();
+
+private:
+	float rotationSpeed;
+	Entity* marble;
+};
+
