3936850c9e95740bd7ce3b2f90f362eae2e5e279
IgnOry <iory@ucm.es>
2020-05-07 18:33:53 +0200

    las estrellas se actualizan

--- VayaCanicastanyazos/VayaCanicastanyazos/src/Components/StarCollision.cpp
+++ VayaCanicastanyazos/VayaCanicastanyazos/src/Components/StarCollision.cpp
@@ -5,7 +5,7 @@
 #include "Entity/Entity.h"
 #include "Entity/Transform.h"
 #include "Audio/AudioManager.h"
-
+#include "GUI/GUI_Manager.h"
 StarCollision::StarCollision(json& args) : Component (args)
 {
 
@@ -26,7 +26,16 @@ void StarCollision::OnCollision(Entity* ent)
 	if (ent->getTag() == "marble")
 	{
 		getEntity()->setEnabled(false);
+		
 		GameManager::getInstance()->addStars(1);
+		
+		if (GameManager::getInstance()->getStars() == 1)
+			GUI_Manager::getInstance()->changeImage("Ingame/Colleccionable_1", "VayaCanicastanhazos/Star_Yes");
+		else if (GameManager::getInstance()->getStars() == 2)
+			GUI_Manager::getInstance()->changeImage("Ingame/Colleccionable_2", "VayaCanicastanhazos/Star_Yes");
+		else if (GameManager::getInstance()->getStars() == 3)
+			GUI_Manager::getInstance()->changeImage("Ingame/Colleccionable_3", "VayaCanicastanhazos/Star_Yes");
+
 		AudioManager::getInstance()->playSound("assets/sound/402Cry.wav", 0);
 		std::cout << GameManager::getInstance()->getStars() << std::endl;
 	}
